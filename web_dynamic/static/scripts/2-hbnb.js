21d3b9ea451660170c1042a35620b3203b50e80e
Saviour Assandoh <asandohs@gmail.com>
2024-08-10 19:00:51 -0700

    API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,4 +13,15 @@ $(function(){
 		
 		$('.amenities h4').text(Object.values(amenityList).join(", "));
 	});
+
+	let url = "http://0.0.0.0:5001/api/v1/status/";
+	$.get(url, function(data)){
+		if (data.status == "OK"){
+			$("div#api_status").addClass("available");
+		} else {
+			$("div#api_status").removeClass("available");
+		}.fail(function(){
+			$('div#api_status').removeClass('available');
+		});
+	}
 });
